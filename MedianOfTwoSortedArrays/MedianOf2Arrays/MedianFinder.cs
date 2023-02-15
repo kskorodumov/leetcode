01d8bea91908cac6520ee1a46fbaf1d8983633f1
Kirill Skorodumov <kirill.skorodumov@securrency.com>
2023-02-15 17:53:35 +0400

    Proper solution for medians

--- MedianOfTwoSortedArrays/MedianOf2Arrays/MedianFinder.cs
+++ MedianOfTwoSortedArrays/MedianOf2Arrays/MedianFinder.cs
@@ -8,258 +8,191 @@ public class MedianFinder
         {
             return FindMedian(nums2);
         }
-
+        
         if (nums2.Length == 0)
         {
             return FindMedian(nums1);
         }
+
         bool evenLength = (nums1.Length + nums2.Length) % 2 == 0;
         int goal = evenLength ? (nums1.Length + nums2.Length) / 2 - 1 : (nums1.Length + nums2.Length) / 2;
-        bool t = false;
-
-        int lesserPart = 0;
-        int nnb = (nums2.Length - 1) / 2;
-        int na = CountLesserElements(nums1, nums2[nnb], 0, nums1.Length - 1, goal - nnb);
-        int resa = na + nnb;
-        if (resa == goal)
-        {
-            if (evenLength)
-            {
-                lesserPart = nums2[nnb];
-                t = true;
-            }
-            else
-            {
-                return nums2[nnb];
-            }
-        }
-        
-        int nna = (nums1.Length - 1) / 2;
-        int nb = CountLesserElements(nums2, nums1[nna], 0, nums2.Length - 1, goal - nna);
-        int resb = nb + nna;
-        if (resb == goal)
-        {
-            if (evenLength)
-            {
-                lesserPart = nums1[nna];
-                t = true;
-            }
-            else
-            {
-                return nums1[nna];
-            }
-        }
         int astart = 0;
         int aend = nums1.Length - 1;
         int bstart = 0;
         int bend = nums2.Length - 1;
-
-        if (!t)
+        int lesserPart;
+        bool lesserPartIn1 = false;
+        while (true)
         {
-            do
+            int ind1 = astart + (aend - astart) / 2;
+            int remainder1 = goal - ind1;
+            int test = TestForLesser(nums2, nums1[ind1], remainder1);
+            if (test == 0)
             {
-                if (resa < goal)
+                if (!evenLength)
                 {
-                    bstart += (bend - bstart) / 2 + 1;
+                    return nums1[ind1];
+                }
+                lesserPartIn1 = true;
+                lesserPart = nums1[ind1];
+                break;
+            }
+
+            if (test > 0)
+            {
+                aend = ind1;
+            }
+            else
+            {
+                if (astart == ind1)
+                {
+                    astart += 1;
                 }
                 else
                 {
-                    bend -= (bend - bstart) / 2 + 1;
+                    astart = ind1;
                 }
+            }
             
-                if (resb < goal)
+            int ind2 = bstart + (bend - bstart) / 2;
+            int remainder2 = goal - ind2;
+            test = TestForLesser(nums1, nums2[ind2], remainder2);
+            
+            if (test == 0)
+            {
+                if (!evenLength)
                 {
-                    astart += (aend - astart) / 2 + 1;
+                    return nums2[ind2];
+                }
+                lesserPart = nums2[ind2];
+                break;
+            }
+
+            if (test > 0)
+            {
+                bend = ind2;
+            }
+            else
+            {
+                if (bstart == ind2)
+                {
+                    bstart += 1;
                 }
                 else
                 {
-                    aend -= (aend - astart) / 2 + 1;
-                }
-
-                nna = astart + (aend - astart) / 2;
-                nnb = bstart + (bend - bstart) / 2;
-                na = CountLesserElements(nums1, nums2[nnb], astart, aend, goal - nnb);
-                resa = na + nnb;
-                if (resa == goal)
-                {
-                    if (evenLength)
-                    {
-                        lesserPart = nums2[nnb];
-                        break;
-                    }
-                    return nums2[nnb];
-                }
-                nb = CountLesserElements(nums2, nums1[nna], bstart, bend, goal - nnb);
-                resb = nb + nna;
-                if (resb == goal)
-                {
-                    if (evenLength)
-                    {
-                        lesserPart = nums1[nna];
-                        break;
-                    }
-                    return nums1[nna];
+                    bstart = ind1;
                 }
             }
-            while (true);
         }
-        astart = 0;
-        aend = nums1.Length - 1;
-        bstart = 0;
-        bend = nums2.Length - 1;
-        
-        nnb = (nums2.Length - 1) / 2;
-        na = CountBiggerElements(nums1, nums2[nnb], 0, nums1.Length - 1);
-        resa = na + bend - nnb;
-        if (resa == goal)
+        //================
+        if (!lesserPartIn1)
         {
-            return (double)(nums2[nnb] + lesserPart) / 2;
-        }
-        
-        nna = (nums1.Length - 1) / 2;
-        nb = CountBiggerElements(nums2, nums1[nna], 0, nums2.Length - 1);
-        resb = nb + aend - nna;
-        if (resb == goal)
-        {
-            return (double)(nums1[nna] + lesserPart) / 2;
+            astart = 0;
+            aend = nums1.Length - 1;
         }
 
-        
-        do
+        if (lesserPartIn1)
         {
-            if (resa > goal)
+            bstart = 0;
+            bend = nums2.Length - 1;
+        }
+        goal += 1;
+        while (true)
+        {
+            int ind1 = astart + (aend - astart) / 2;
+            int remainder1 = goal - ind1;
+            int test = TestForLesser(nums2, nums1[ind1], remainder1);
+            if (test == 0)
             {
-                
-                bstart += (bend - bstart) / 2 + 1;
+                return (double)(lesserPart + nums1[ind1]) / 2;
+            }
+
+            if (test > 0)
+            {
+                aend = ind1;
             }
             else
             {
-                bend -= (bend - bstart) / 2 + 1;
+                if (astart == ind1 && astart != nums1.Length - 1)
+                {
+                    astart += 1;
+                }
+                else
+                {
+                    astart = ind1;
+                }
             }
             
-            if (resb > goal)
-            {
-                astart += (aend - astart) / 2 + 1;
-            }
-            else
-            {
-                aend -= (aend - astart) / 2 + 1;
-                
-            }
-
-            nna = astart + (aend - astart) / 2;
-            nnb = bstart + (bend - bstart) / 2;
-            na = CountBiggerElements(nums1, nums2[nnb], astart, aend);
-            resa = na + nums2.Length - nnb - 1;
-            if (resa == goal)
-            {
-                return (double)(nums2[nnb] + lesserPart) / 2;
-            }
-            nb = CountBiggerElements(nums2, nums1[nna], bstart, bend);
-            resb = nb + nums1.Length - nna - 1;
-            if (resb == goal)
-            {
-                return (double)(nums1[nna] + lesserPart) / 2;
-            }
-        }
-        while (true);
-    }
-
-    public static int CountLesserElements(int[] a, double x, int start, int end, int goal)
-    {
-        int startcopy = start;
-        int endcopy = end;
-        int l = end - start + 1;
-        int k = start + l / 2;
-
-        if (start > end) return 0;
-        if (l == 1)
-        {
-            return a[k] <= x ? start + 1 : start;
-        }
-        if (l == 2)
-        {
-            k -= 1;
-        }
-        while (!(a[k] <= x && a[k + 1] >= x))
-        {
-            if (a[k] >= x)
-            {
-                if (k == startcopy) return startcopy;
-                end = k;
-                k = start + (k - start) / 2;
-            }
-            else
-            {
-                if (k + 1 == endcopy) return endcopy + 1;
-                start = k;
-                k = end - (end - k) / 2;
-            }
-        }
-
-        if (a[k] != x) return k + 1;
-
-        while (k != goal)
-        {
-            if (k < goal)
-                k++;
-            else
-                k--;
+            int ind2 = bstart + (bend - bstart) / 2;
+            int remainder2 = goal - ind2;
+            test = TestForLesser(nums1, nums2[ind2], remainder2);
             
-            if (a[k] != x) break;
-        }
+            if (test == 0)
+            {
+                return (double)(lesserPart + nums2[ind2]) / 2;
+            }
 
-        return k;
+            if (test > 0)
+            {
+                bend = ind2;
+            }
+            else
+            {
+                if (bstart == ind2 && bstart != nums2.Length - 1)
+                {
+                    bstart += 1;
+                }
+                else
+                {
+                    bstart = ind1;
+                }
+            }
+        }
     }
+
     
-    public static int CountBiggerElements(int[] a, double x, int start, int end)
-    {
-        int startcopy = start;
-        int endcopy = end;
-        int l = end - start + 1;
-        int k = start + l / 2;
-
-        if (start > end) return 0;
-        if (l == 1)
-        {
-            if (x <= a[start])
-            {
-                return a.Length - start;
-            }
-            return a.Length - start - 1;
-        }
-        if (l == 2)
-        {
-            k -= 1;
-        }
-        while (!(a[k] <= x && a[k + 1] >= x))
-        {
-            if (a[k] >= x)
-            {
-                if (k == startcopy) return endcopy - startcopy + 1;
-                end = k;
-                k = start + (k - start) / 2;
-            }
-            else
-            {
-                if (k + 1 == endcopy) return 0;
-                start = k;
-                k = end - (end - k) / 2;
-            }
-        }
-
-        if (a[k] != x) return endcopy - k;
-
-        while (k > 0 && a[k] == x)
-        {
-            k--;
-        }
-
-        return endcopy - k;
-    }
-
     private static double FindMedian(int[] a)
     {
         return a.Length % 2 == 0 ? (double)(a[(a.Length - 1) / 2] + a[(a.Length - 1) / 2 + 1]) / 2 : a[(a.Length - 1) / 2];
+    }
+    
+    public static int TestForLesser(int[] a, int x, int goal)
+    {
+        if (a.Length == 1)
+        {
+            if (a[0] <= x && goal == 1) return 0;
+            if (a[0] > x && goal == 0) return 0;
+
+            if (a[0] > x) return -1;
+            return 1;
+        }
+
+        if (goal == 0)
+        {
+            if (a[0] >= x) return 0;
+            return 1;
+        }
+
+        if (goal >= a.Length)
+        {
+            if (goal == a.Length)
+            {
+                if (a[a.Length - 1] <= x) return 0;
+                return -1;
+            }
+
+            return 1;
+        }
+        if (a[goal - 1] <= x && a[goal] >= x)
+        {
+            return 0;
+        }
+
+        if (a[goal - 1] > x)
+        {
+            return -1;// x is too small
+        }
+
+        return 1; // x is too big
     }
 } 
--- MedianOfTwoSortedArrays/MedianOf2Arrays/Tests/MedianFinderTests.cs
+++ MedianOfTwoSortedArrays/MedianOf2Arrays/Tests/MedianFinderTests.cs
@@ -10,76 +10,26 @@ public class MedianFinderTests
     [InlineData(new[] {1, 2, 3, 5}, new [] {0}, 2)]
     [InlineData(new[] {1, 2, 3, 5, 6}, new [] {0, 7}, 3)]
     [InlineData(new[] {1, 2, 3, 50, 53, 300, 301}, new [] {100, 200}, 53)]
-    [InlineData(new[] {1, 2, 3, 50, 53, 300, 301}, new [] {0, 100, 200, 1000, 1001, 1002}, 100)]
-    [InlineData(new[] {1, 5}, new [] {2, 3}, 2.5)]
-    [InlineData(new[] {1, 5, 8, 10}, new [] {2, 3}, 4)]
+    [InlineData(new[] {1, 2, 3, 50, 53, 300, 301}, new [ ] {0, 100, 200, 1000, 1001, 1002}, 100)]
     [InlineData(new[] {1, 3}, new [] {2}, 2)]
-    [InlineData(new[] {1, 2}, new [] {3, 4}, 2.5)]
     [InlineData(new[] {1, 2}, new [] {3}, 2)]
-    [InlineData(new[] {1}, new [] {3}, 2)]
     [InlineData(new[] {5}, new [] {100, 200}, 100)]
-    [InlineData(new[] {1, 2, 3}, new [] {100, 200, 201}, 51.5)]
     [InlineData(new[] {1, 2, 3, 4}, new [] {100}, 3)]
-    [InlineData(new[] {2, 2, 2}, new [] {3, 3, 3}, 2.5)]
     [InlineData(new[] {2, 2, 2}, new [] {3, 3}, 2)]
-    [InlineData(new[] {1, 2, 3, 5}, new [] {2, 3}, 2.5)]
     [InlineData(new[] {1, 2, 3, 5}, new int[] {}, 2.5)]
     [InlineData(new[] {1, 2, 3}, new int[] {}, 2)]
+    [InlineData(new[] {0,0,0,0}, new [] {-1,0,0,0,0,0,1}, 0)]
+    [InlineData(new[] {2, 2, 2}, new [] {3, 3, 3}, 2.5)]
+    [InlineData(new[] {1, 5}, new [] {2, 3}, 2.5)]
+    [InlineData(new[] {1, 5, 8, 10}, new [] {2, 3}, 4)]
+    [InlineData(new[] {1, 2}, new [] {3, 4}, 2.5)]
+    [InlineData(new[] {1}, new [] {3}, 2)]
+    [InlineData(new[] {1, 2, 3}, new [] {100, 200, 201}, 51.5)]
     [InlineData(new[] {0,0,0,0,0}, new [] {-1,0,0,0,0,0,1}, 0)]
+    [InlineData(new[] {1, 2, 3, 5}, new [] {2, 3}, 2.5)]
     public void FindTwosArraysMedianTest(int[] a, int[] b, double expected)
     {
         var actual = MedianFinder.FindMedianSortedArrays(a, b);
-        actual.Should().Be(expected);
-    }
-
-    [Theory]
-    [InlineData(new[] {1, 2, 3}, 2, 0, 2, 2, 0)]
-    [InlineData(new[] {1, 2, 3}, 4, 0, 2, 3, 0)]
-    [InlineData(new[] {1, 2, 3}, 0, 0, 2, 0, 0)]
-    [InlineData(new[] {1, 3, 5}, 2, 0, 2, 1, 0)]
-    [InlineData(new[] {1, 2, 3, 5}, 4, 0, 2, 3, 0)]
-    [InlineData(new[] {1}, 4, 0, 0, 1, 0)]
-    [InlineData(new[] {2}, 1, 0, 0, 0, 0)]
-    [InlineData(new[] {1, 2, 3, 5, 8, 12, 15}, 9, 3, 6, 5, 0)]
-    [InlineData(new[] {1, 2, 3, 5, 8, 12, 15}, 9, 2, 4, 5, 0)]
-    [InlineData(new[] {1, 2}, 9, 0, 1, 2, 0)]
-    [InlineData(new[] {1, 3}, 2, 0, 1, 1, 0)]
-    [InlineData(new[] {1, 3}, 0, 0, 1, 0, 0)]
-    [InlineData(new[] {1, 3}, 0, 0, 0, 0, 0)]
-    [InlineData(new[] {1, 3}, 1.5, 0, 1, 1, 0)]
-    [InlineData(new[] {1, 2}, 3, 1, 1, 2, 0)]
-    [InlineData(new[] {1, 2}, 2, 1, 1, 2, 0)]
-    [InlineData(new[] {1, 2, 3, 5}, 2, 0, 3, 2, 0)]
-    [InlineData(new[] {1, 2, 2, 2, 3, 5}, 2, 0, 5, 4, 0)]
-    [InlineData(new[] {1, 2, 2, 2, 2, 3, 5}, 2, 0, 6, 4, 4)]
-    [InlineData(new[] {1, 2, 2, 2, 2, 3, 5}, 2, 0, 6, 5, 5)]
-    public void CountLesserElementsTest(int[] a, int x, int start, int end, int expected, int goal)
-    {
-        var actual = MedianFinder.CountLesserElements(a, x, start, end, goal);
-        actual.Should().Be(expected);
-    }
-    
-    [Theory]
-    [InlineData(new[] {1, 2, 3}, 2, 0, 2, 2)]
-    [InlineData(new[] {1, 2, 3}, 4, 0, 2, 0)]
-    [InlineData(new[] {1, 2, 3}, 0, 0, 2, 3)]
-    [InlineData(new[] {1, 3, 5}, 2, 0, 2, 2)]
-    [InlineData(new[] {1, 2, 3, 5}, 4, 0, 2, 0)]
-    [InlineData(new[] {1}, 4, 0, 0, 0)]
-    [InlineData(new[] {2}, 1, 0, 0, 1)]
-    [InlineData(new[] {1, 2, 3, 5, 8, 12, 15}, 9, 3, 6, 2)]
-    [InlineData(new[] {1, 2, 3, 5, 8, 12, 15}, 9, 2, 4, 0)]
-    [InlineData(new[] {1, 2}, 9, 0, 1, 0)]
-    [InlineData(new[] {1, 3}, 2, 0, 1, 1)]
-    [InlineData(new[] {1, 3}, 0, 0, 1, 2)]
-    [InlineData(new[] {1, 3}, 0, 0, 0, 2)]
-    [InlineData(new[] {1, 3}, 1.5, 0, 1, 1)]
-    [InlineData(new[] {1, 3}, 4, 1, 1, 0)]
-    [InlineData(new[] {1, 3}, 2, 1, 1, 1)]
-    [InlineData(new[] {1, 3, 3, 3}, 3, 0, 3, 3)]
-    public void CountBiggerElementsTest(int[] a, int x, int start, int end, int expected)
-    {
-        var actual = MedianFinder.CountBiggerElements(a, x, start, end);
         actual.Should().Be(expected);
     }
 }
